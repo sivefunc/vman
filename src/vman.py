c46033171537c0a178e1a957a0beb865cef633ca
sivefunc <sivefunc@tuta.io>
2025-06-17 13:17:41 -0400

    feat: string to url mapping

--- src/vman.py
+++ src/vman.py
@@ -2,7 +2,21 @@ import json
 import os
 import pathlib
 
+import sys
+
 from _version import __version__
+
+def get_url(urls: dict) -> list[bool, str]:
+    try:
+        result = [True, urls[sys.argv[1]]]
+    
+    except IndexError:
+        result = [False, "No video Provided"]
+
+    except KeyError:
+        result = [False, f"Video for '{sys.argv[1]}' does not exist"]
+
+    return result
 
 def main():
     urls_path = os.path.join(
@@ -13,8 +27,13 @@ def main():
     with open(urls_path, 'r') as json_fp:
         urls = json.load(json_fp)
 
+    success, url = get_url(urls)
+    if not success:
+        print(url)
+        exit()
+
     print(__version__)
-    print(urls)
+    print(url)
 
 if __name__ == '__main__':
     main()
