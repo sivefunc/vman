e878b13b1516b96c47bde415f0fee99f5ab536f2
sivefunc <sivefunc@tuta.io>
2025-06-17 15:49:21 -0400

    feat: argparse instead of manually sys.argv

---
+++ src/term_args.py
@@ -0,0 +1,58 @@
+""" Single function just to parse CLI args """
+
+import sys
+import argparse
+
+from _version import __version__
+
+def term_args() -> argparse.Namespace:
+    """
+    Analyzes sys.argv to map the text to options.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    term args : argparse.Namespace
+                C struct in python where each member is an option. 
+    """
+
+    # NO ARGUMENTS
+    if len(sys.argv) == 1:
+        print(
+            "What video manual do you want?\n"
+            "For example, try 'vman ls'."
+        )
+        sys.exit(0)
+
+    parser = argparse.ArgumentParser(
+        prog="vman",
+        formatter_class=argparse.RawTextHelpFormatter,
+        usage='%(prog)s video [options]',
+        description="vman - Video Man Pages"
+    )
+
+    parser.add_argument(
+        '-v','--version',
+        action='version',
+        version=f"""
+%(prog)s v{__version__}
+Copyright (C) 2025 Sivefunc
+License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by a human"""
+    )
+
+    parser.add_argument(
+        'video',
+        help="Video to consult in the video manual, e.g 'ls'",
+        type=str,
+        metavar="[VIDEO]"
+    )
+
+    args = parser.parse_args()
+    return args
--- src/vman.py
+++ src/vman.py
@@ -4,23 +4,11 @@ import shlex
 import pathlib
 import subprocess
 
-import sys
-
 from _version import __version__
-
-def get_url(urls: dict) -> list[bool, str]:
-    try:
-        result = [True, urls[sys.argv[1]]]
-    
-    except IndexError:
-        result = [False, "No video Provided"]
-
-    except KeyError:
-        result = [False, f"Video for '{sys.argv[1]}' does not exist"]
-
-    return result
+import term_args
 
 def main():
+    t_args = term_args.term_args()
     urls_path = os.path.join(
         pathlib.Path(__file__).parent.resolve(),
         'urls.json'
@@ -29,10 +17,12 @@ def main():
     with open(urls_path, 'r') as json_fp:
         urls = json.load(json_fp)
 
-    success, url = get_url(urls)
-    if not success:
-        print(url)
-        exit()
+    try:
+        url = urls[t_args.video]
+
+    except KeyError:
+        print(f"No video manual for {t_args.video}")
+        exit(16)
 
     subprocess.run(shlex.split(f"xdg-open '{url}'"), check=True)
 
