7df360971fb22ce2ef8f4581562a85b8453e80ac
sivefunc <sivefunc@tuta.io>
2025-06-16 20:19:12 -0400

    feat: json read

--- src/vman.py
+++ src/vman.py
@@ -1,7 +1,20 @@
+import json
+import os
+import pathlib
+
 from _version import __version__
 
 def main():
+    urls_path = os.path.join(
+        pathlib.Path(__file__).parent.resolve(),
+        'urls.json'
+    )
+
+    with open(urls_path, 'r') as json_fp:
+        urls = json.load(json_fp)
+
     print(__version__)
+    print(urls)
 
 if __name__ == '__main__':
     main()
