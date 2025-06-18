532f781f132375ae19ab25432ae59c461b39a96b
sivefunc <sivefunc@tuta.io>
2025-06-18 16:13:31 -0400

    docs:

--- vman/vman.py
+++ vman/vman.py
@@ -25,22 +25,28 @@ def main():
     """ Entry Point Function """
     try:
         terminal = term_args.term_args()
+
+        # Loading Files
         config = load_config_or_exit()
         urls_path = get_urls_path_or_exit(config)
         urls = load_urls_or_exit(urls_path)
             
+        # Print all the Urls
         if terminal.urls:
             print(json.dumps(urls, indent=4))
             sys.exit(constants.SUCCESS)
 
+        # Get Video made by Author
         author = get_author_or_exit(terminal.author, config.get('author'))
         author_urls = get_author_urls_or_exit(author, urls)
         video_url = get_video_url_or_exit(terminal.video, author, author_urls)
 
+        # Print only the video url
         if terminal.only_url:
             print(video_url)
             sys.exit(constants.SUCCESS)
 
+        # Play the video
         media_player = get_media_player_or_exit(terminal.player,
                                                 config.get('media_player'))
         play_video(media_player, video_url)
