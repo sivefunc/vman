03ebc904ded395ac83e7141e1fcb2e46282168d3
sivefunc <sivefunc@tuta.io>
2025-06-18 11:59:33 -0400

    feat: exception handling

--- src/vman.py
+++ src/vman.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 
 from _version import __version__
 
@@ -16,21 +17,31 @@ from utils import (
 )
 
 def main():
-    t_args = term_args.term_args()
-    config = load_config_or_exit()
-    urls_path = get_urls_path_or_exit(config)
-    urls = load_urls_or_exit(urls_path)
-        
-    if t_args.urls:
-        print(json.dumps(urls, indent=4))
-        sys.exit(constants.SUCCESS)
+    try:
+        t_args = term_args.term_args()
+        config = load_config_or_exit()
+        urls_path = get_urls_path_or_exit(config)
+        urls = load_urls_or_exit(urls_path)
+            
+        if t_args.urls:
+            print(json.dumps(urls, indent=4))
+            sys.exit(constants.SUCCESS)
 
-    author = get_author_or_exit(t_args.author, config.get('author'))
-    author_urls = get_author_urls_or_exit(author, urls)
-    video_url = get_video_url_or_exit(t_args.video, author, author_urls)
-    media_player = get_media_player_or_exit(t_args.player,
-                                            config.get('media_player'))
-    play_video(media_player, video_url)
+        author = get_author_or_exit(t_args.author, config.get('author'))
+        author_urls = get_author_urls_or_exit(author, urls)
+        video_url = get_video_url_or_exit(t_args.video, author, author_urls)
+        media_player = get_media_player_or_exit(t_args.player,
+                                                config.get('media_player'))
+        play_video(media_player, video_url)
+
+    except KeyboardInterrupt:
+        sys.exit(constants.USER_ERROR)
+
+    except Exception:
+        traceback.print_exc()
+        sys.exit(constants.OPERATIONAL_ERROR)
+
+    sys.exit(constants.SUCCESS)
 
 if __name__ == '__main__':
     main()
